ad02bdb98f0f7303dd47e542b71cdb3de3143dd5
jimmyliaoviva <jimmy.liao86@gmail.com>
2021-07-16 18:43:29 +0800

    comment voteCommand

--- src/commands/game/VoteCommand.js
+++ src/commands/game/VoteCommand.js
@@ -1,15 +1,16 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
-const { spygameData } = require('./spygameVar');
+// const BaseCommand = require('../../utils/structures/BaseCommand');
+// const { spygameData } = require('./spygameVar');
 
-module.exports = class VoteCommand extends BaseCommand {
-  constructor() {
-    super('vote', 'game', []);
-  }
+// module.exports = class VoteCommand extends BaseCommand {
+//   constructor() {
+//     super('vote', 'game', []);
+//   }
 
-  run(client, message, args) {
-    const data = spygameData.get(message.guild.id);
-    const member = data.roles.get(args[0].substring(3,21 ));
-    message.channel.send(`他是 ${ member.role }`);
+//   run(client, message, args) {
+//     const data = spygameData.get(message.guild.id);
+//     const member = data.roles.get(args[0].substring(3,21 ));
+//     message.channel.send(`他是 ${ member.role }`);
+
     // data.roles.forEach((item, index) => {
     //   if (args[0].substring(3,21 )==item.user.id) {
     //     message.channel.send(`他是 ${ item.id }`);
